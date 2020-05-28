eb163e8c984ccb37db85e2aa3cc9f81d36e9acee
BishopBD <briguy032@gmail.com>
2020-05-28 18:03:39 +0000

    Changes made before after-lunch break

--- BooksMvc/Controllers/BooksController.cs
+++ BooksMvc/Controllers/BooksController.cs
@@ -1,16 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BooksMvc.Domain;
+using BooksMvc.Models.Books;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace BooksMvc.Controllers
 {
     public class BooksController : Controller
     {
-        public IActionResult Index()
+        private readonly BooksDataContext _dataContext;
+
+        public BooksController(BooksDataContext dataContext)
         {
-            return View();
+            _dataContext = dataContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //var response = 
+            //    await 
+            //        _dataContext
+            //            .Books
+            //            .Where(b => b.InInventory)
+            //            .ToListAsync();
+
+            //return View(response);
+
+            var response = new GetBooksResponseModel
+            {
+                Books = await _dataContext.Books.Where(b => b.InInventory).Select(b => new BooksResponseItemModel
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author
+                }).ToListAsync(),
+                NumberOfBooksInInventory = await _dataContext.Books.CountAsync(b => b.InInventory),
+                NumberOfBooksNotInInventory = await _dataContext.Books.CountAsync(b => b.InInventory == false)
+            };
+
+            return View(response);
         }
     }
 }
