23d94ca76cf48671e871ab6779b4ca5c982141ab
BishopBD <briguy032@gmail.com>
2020-05-28 19:47:43 +0000

    End Of Day One!!!

--- BooksMvc/Controllers/BooksController.cs
+++ BooksMvc/Controllers/BooksController.cs
@@ -16,7 +16,35 @@ namespace BooksMvc.Controllers
             _dataContext = dataContext;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Details(int id)
+        {
+            var response = await 
+                _dataContext
+                    .Books
+                    .Where(b => b.Id == id)
+                    .Select(b => new GetSingleBookResponseModel
+                    {
+                        Id = b.Id,
+                        Title = b.Title,
+                        Author = b.Author,
+                        InInventory = b.InInventory,
+                        NumberOfPages = b.NumberOfPages
+                    })
+                    .SingleOrDefaultAsync();
+            if(response == null)
+            {
+                return NotFound("No Book with that Id");
+            }
+            else
+            {
+                return View(response);
+            }
+        }
+
+        //GET /books
+        //GET /books/index
+        //GET /books?showall=true
+        public async Task<IActionResult> Index([FromQuery] bool showAll = false)
         {
             //var response = 
             //    await 
@@ -38,6 +66,21 @@ namespace BooksMvc.Controllers
                 NumberOfBooksInInventory = await _dataContext.Books.CountAsync(b => b.InInventory),
                 NumberOfBooksNotInInventory = await _dataContext.Books.CountAsync(b => b.InInventory == false)
             };
+
+            if (showAll)
+            {
+                response.BooksNotInInventory = await 
+                    _dataContext
+                        .Books
+                        .Where(b => b.InInventory == false)
+                        .Select(b => new BooksResponseItemModel
+                        {
+                            Id = b.Id,
+                            Title = b.Title,
+                            Author = b.Author
+                        })
+                        .ToListAsync();
+            }
 
             return View(response);
         }
---
+++ BooksMvc/Models/Books/GetSingleBookResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksMvc.Models.Books
+{
+    public class GetSingleBookResponseModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [DisplayName("Available")]
+        public bool InInventory { get; set; }
+        [DisplayName("Number Of Pages")]
+        public int NumberOfPages { get; set; }
+    }
+}
